80f4be655eaad380c2359a464fbfe3257b43925c
DESKTOP-BP9CRHJ\admin <123123123123@qq.com>
2020-06-06 19:50:58 +0800

    no message

---
+++ vs/repos/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using IsJson;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String str = "{name:123}";
+            Console.WriteLine(JsonSplit.IsJsonStart( ref str));
+        }
+    }
+}
---
+++ vs/repos/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string folder_path=String.Empty;
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                folder_path = ofd.FileName;
+            }
+            dataGridView1.DataSource = null; //每次打开清空内容
+            DataTable dt = ReadExcelToTable(folder_path);
+            dataGridView1.DataSource = dt;
+        }
+        //根据excle的路径把第一个sheel中的内容放入datatable
+        public static DataTable ReadExcelToTable(string path)//excel存放的路径
+        {
+            try
+            {
+
+                //连接字符串
+                string connstring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties='Excel 8.0;HDR=NO;IMEX=1';"; // Office 07及以上版本 不能出现多余的空格 而且分号注意
+                //string connstring = Provider=Microsoft.JET.OLEDB.4.0;Data Source=" + path + ";Extended Properties='Excel 8.0;HDR=NO;IMEX=1';"; //Office 07以下版本 
+                using (OleDbConnection conn = new OleDbConnection(connstring))
+                {
+                    conn.Open();
+                    DataTable sheetsName = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "Table" }); //得到所有sheet的名字
+                    string firstSheetName = sheetsName.Rows[0][2].ToString(); //得到第一个sheet的名字
+                    string sql = string.Format("SELECT * FROM [{0}]", firstSheetName); //查询字符串                    //string sql = string.Format("SELECT * FROM [{0}] WHERE [日期] is not null", firstSheetName); //查询字符串
+                    OleDbDataAdapter ada = new OleDbDataAdapter(sql, connstring);
+                    DataSet set = new DataSet();
+                    ada.Fill(set);
+                    return set.Tables[0];
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+
+    }
+}
